bc35915e1a66fbfc90851d47aaccb7da8c69f30b
jina <jina0211@wizzpay.co.kr>
2021-03-12 12:31:50 +0900

    '5'

--- src/main/java/com/test/index/IndexController.java
+++ src/main/java/com/test/index/IndexController.java
@@ -1,15 +1,10 @@
 package com.test.index;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
